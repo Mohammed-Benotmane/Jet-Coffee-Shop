391a45394cb054216a2d1ada04e42ee43a6b4cdd
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-09-06 20:04:51 +0200

    - adding appbar login - adding TextField

--- lib/screens/login-form.dart
+++ lib/screens/login-form.dart
@@ -1,3 +1,4 @@
+import 'package:coffeshopapp/const.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,8 +11,41 @@ class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        leading: Icon(Icons.keyboard_arrow_left),
+        elevation: 0,
+        leading: Icon(Icons.keyboard_arrow_left,color: Colors.brown,),
+        backgroundColor: Colors.white,
+        title: Text("Login",style: TextStyle(color: Colors.brown),),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30,80,30,30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Welcome back!",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400, color: Colors.brown),
+            ),
+            SizedBox(height: 30),
+            Text("Email",style: TextStyle(color: darkBrown)),
+            TextField(
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: darkBrown),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: darkBrown),
+                ),
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: darkBrown),
+                ),
+              ),
+              cursorColor: darkBrown,
+            ),
+          ],
+        ),
       ),
     );
   }
