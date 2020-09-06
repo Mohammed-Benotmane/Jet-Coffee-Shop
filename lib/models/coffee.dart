ec87656b2c37125edec2d6db8593b7f2820aaed9
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-09-06 22:27:10 +0200

    - adding coffee Model

---
+++ lib/models/coffee.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Coffee{
+  Icon coffeeIcon;
+  String name;
+  int price;
+
+  Coffee({this.coffeeIcon,this.name,this.price});
+}
--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -9,7 +9,25 @@ class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        leading: Icon(
+          Icons.keyboard_arrow_left,
+          color: Colors.brown,
+        ),
+        backgroundColor: Colors.white,
+        title: Text(
+          "Menu",
+          style: TextStyle(fontFamily: 'Raleway',color: Colors.brown),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          
+        ],
+      ),
     );
   }
 }
