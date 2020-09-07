97937490c2a5887987d9be6e3ba5179d4808199f
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-09-07 20:53:40 +0200

    - Completing menu Screen

--- lib/screens/menu_list.dart
+++ lib/screens/menu_list.dart
@@ -2,6 +2,7 @@ import 'package:coffeshopapp/const.dart';
 import 'package:coffeshopapp/models/coffee.dart';
 import 'package:flutter/material.dart';
 import 'file:///C:/Users/S/Documents/Flutter%20projects/coffe_shop_app/lib/icons/my_flutter_app_icons.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MenuList extends StatefulWidget {
   @override
@@ -10,10 +11,11 @@ class MenuList extends StatefulWidget {
 
 class _MenuListState extends State<MenuList> {
   List<Coffee> coffees = [
-    Coffee(coffeeIcon: Icon(MyFlutterApp.cup, size: 20, color: brown), name: "Espresso", price: 500),
-    Coffee(coffeeIcon: Icon(MyFlutterApp.beer, size: 20, color: brown), name: "Cappuccino", price: 600),
-    Coffee(coffeeIcon: Icon(MyFlutterApp.coffee_cup, size: 20, color: brown), name: "Macciato", price: 350),
-    Coffee(coffeeIcon: Icon(MyFlutterApp.coffee_mug, size: 20, color: brown), name: "Latte", price: 150),
+    Coffee(coffeeIcon: Icon(MyFlutterApp.cup, size: 40, color: brown), name: "Espresso", price: 500),
+    Coffee(coffeeIcon: Icon(MyFlutterApp.beer, size: 40, color: brown), name: "Cappuccino", price: 600),
+    Coffee(coffeeIcon: Icon(MyFlutterApp.coffee_cup, size: 40, color: brown), name: "Macciato", price: 350),
+    Coffee(coffeeIcon: Icon(MyFlutterApp.coffee_mug, size: 40, color: brown), name: "Latte", price: 150),
+    Coffee(coffeeIcon: Icon(FontAwesomeIcons.coffee, size: 35, color: brown), name: "Mocha", price: 150),
   ];
 
   @override
@@ -26,11 +28,18 @@ class _MenuListState extends State<MenuList> {
             shrinkWrap: true,
             itemCount: coffees.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(coffees[index].name),
-                leading: coffees[index].coffeeIcon,
-                trailing: Icon(Icons.keyboard_arrow_right),
+              return Column(
+                children: <Widget>[
+                  ListTile(
+                    contentPadding: EdgeInsets.all(20),
+                    title: Text(coffees[index].name),
+                    leading: coffees[index].coffeeIcon,
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
+                  Divider(height: 1, color: Colors.grey.shade400),
+                ],
               );
+
             },
           ),
         ],
